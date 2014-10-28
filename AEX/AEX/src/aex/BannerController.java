e6c6aa7773e8d01d355e40205c23404f3b5d629e
RoyTimmers <roytimmers_7@hotmail.com>
2014-10-28 11:30:25 +0100

    AEX banner skelet

---
+++ AEX/AEX/src/aex/BannerController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aex;
+
+/**
+ *
+ * @author Roy
+ */
+public class BannerController {
+    AEXBanner banner;
+    IEffectenbeurs effectenbeurs;
+    
+    public BannerController(AEXBanner b, IEffectenbeurs e)
+    {
+        banner = b;
+        effectenbeurs = e;
+    }
+    
+}
---
+++ AEX/AEX/src/aex/MockEffectenbeurs.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aex;
+
+/**
+ *
+ * @author Roy
+ */
+public class MockEffectenbeurs {
+    
+    
+    public void koersWisseling()
+    {
+        
+    }
+    
+}
